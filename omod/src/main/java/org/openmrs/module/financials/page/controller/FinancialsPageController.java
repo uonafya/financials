4d2564e58a3556cd1236a175378c60b689e52c9e
ningosi <nicotwendelee@gmail.com>
2021-04-06 09:09:36 +0300

    provide required values for the person attributes

--- omod/src/main/java/org/openmrs/module/financials/page/controller/FinancialsPageController.java
+++ omod/src/main/java/org/openmrs/module/financials/page/controller/FinancialsPageController.java
@@ -1,5 +1,7 @@
 package org.openmrs.module.financials.page.controller;
 
+import org.openmrs.PersonAttributeType;
+import org.openmrs.api.PersonService;
 import org.openmrs.api.context.Context;
 import org.openmrs.module.financials.PatientBillSummary;
 import org.openmrs.module.hospitalcore.BillingService;
@@ -17,23 +19,30 @@ public class FinancialsPageController {
 	public void controller(UiUtils ui, PageModel model) {
 		
 		List<PatientServiceBill> allBils = Context.getService(BillingService.class).getAllPatientServiceBill();
+		PersonService personService = Context.getPersonService();
 		List<PatientBillSummary> allBills = new ArrayList<PatientBillSummary>();
-		PatientBillSummary patientBillSummary = new PatientBillSummary();
+		
+		//get person attribute for patient category and sub category
+		PersonAttributeType paymentCategory = personService
+		        .getPersonAttributeTypeByUuid("09cd268a-f0f5-11ea-99a8-b3467ddbf779");
+		PersonAttributeType paymentSubCategory = personService
+		        .getPersonAttributeTypeByUuid("972a32aa-6159-11eb-bc2d-9785fed39154");
 		
 		for (PatientServiceBill patientServiceBill : allBils) {
+			PatientBillSummary patientBillSummary = new PatientBillSummary();
 			patientBillSummary.setBillId(patientServiceBill.getPatientServiceBillId());
-			patientBillSummary.setPatient(patientServiceBill.getPatient().getFamilyName());
-			patientBillSummary.setCategory(patientServiceBill.getPatientCategory());
-			patientBillSummary.setSubCategory(patientServiceBill.getPatientSubCategory());
+			patientBillSummary.setPatient(patientServiceBill.getPatient().getPersonName().getFullName());
+			patientBillSummary.setCategory(patientServiceBill.getPatient().getAttribute(paymentCategory).getValue());
+			patientBillSummary.setSubCategory(patientServiceBill.getPatient().getAttribute(paymentSubCategory).getValue());
 			patientBillSummary.setWaiver(String.valueOf(patientServiceBill.getWaiverAmount()));
 			patientBillSummary.setActualAmount(String.valueOf(patientServiceBill.getActualAmount()));
 			patientBillSummary.setPaidAmount(String.valueOf(patientServiceBill.getAmount()));
 			patientBillSummary.setRebate(String.valueOf(patientServiceBill.getRebateAmount()));
 			patientBillSummary.setTransactionDate(String.valueOf(patientServiceBill.getReceipt().getPaidDate()));
 			patientBillSummary.setIdentifier(patientServiceBill.getPatient().getPatientIdentifier().getIdentifier());
+			//add this build object to the list
+			allBills.add(patientBillSummary);
 		}
-		//add this build object to the list
-		allBills.add(patientBillSummary);
 		
 		model.addAttribute("bills", allBills);
 	}
--- omod/src/main/webapp/pages/financials.gsp
+++ omod/src/main/webapp/pages/financials.gsp
@@ -20,19 +20,24 @@
          background-color: lightgrey;
          font-size: 16px;
     }
+    .formatp{
+        text-align: center;
+        background-color: lightgrey;
+        font-size: 16px;
+    }
 </style>
 
 <div class="ke-page-content">
     <div>
-        <p align="center">
+        <p align="center" class="formatp">
             <b>Financial Report</b>
         </p>
     </div>
-        <table>
+        <table border="0" cellpadding="0" cellspacing="10">
             <tr>
                 <td valign="top">
                     <span class="format">Cummulative Revenue</span>
-                    <table id="details">
+                    <table id="details" width="40%">
                         <thead>
                         <tr>
                             <th>Service bill #</th>
@@ -42,7 +47,6 @@
                             <th>Patient Category</th>
                             <th>Patient SubCategory</th>
                             <th>Waiver Amount</th>
-                            <th>Rebate Amount</th>
                             <th>Actual Amount</th>
                             <th>Paid Amount</th>
                         </tr>
@@ -57,7 +61,6 @@
                             <td>${it.category}</td>
                             <td>${it.subCategory}</td>
                             <td>${it.waiver}</td>
-                            <td>${it.rebate}</td>
                             <td>${it.actualAmount}</td>
                             <td>${it.paidAmount}</td>
                         </tr>
@@ -66,12 +69,15 @@
                     </table>
                 </td>
                 <td>
-                    <table id="servicePoint" valign="top">
+                    <table id="servicePoint" valign="top" width="40%">
                         <span class="format">Revenue per departiments</span>
                         <thead>
                             <tr>
                                 <th>Depatiment</th>
                                 <th>Amount</th>
+                                <th>Cols1</th>
+                                <th>Cols2</th>
+                                <th>Cols3</th>
                             </tr>
                         </thead>
                     </table>
