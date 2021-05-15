138457da8b71d5a0283254fdd6ab74fbbe0c4df8
ningosi <nicotwendelee@gmail.com>
2021-05-15 23:44:30 +0300

    Add the malaria report definition and basic columns

---
+++ api/src/main/java/org/openmrs/module/financials/reports/SetupMalariaReport.java
@@ -0,0 +1,107 @@
+package org.openmrs.module.financials.reports;
+
+import org.openmrs.PatientIdentifierType;
+import org.openmrs.module.financials.reporting.library.dataset.CommonDatasetDefinition;
+import org.openmrs.module.kenyacore.report.HybridReportDescriptor;
+import org.openmrs.module.kenyacore.report.ReportDescriptor;
+import org.openmrs.module.kenyacore.report.ReportUtils;
+import org.openmrs.module.kenyacore.report.builder.AbstractHybridReportBuilder;
+import org.openmrs.module.kenyacore.report.builder.Builds;
+import org.openmrs.module.kenyaemr.metadata.HivMetadata;
+import org.openmrs.module.metadatadeploy.MetadataUtils;
+import org.openmrs.module.reporting.cohort.definition.CohortDefinition;
+import org.openmrs.module.reporting.cohort.definition.SqlCohortDefinition;
+import org.openmrs.module.reporting.data.DataDefinition;
+import org.openmrs.module.reporting.data.converter.BirthdateConverter;
+import org.openmrs.module.reporting.data.converter.DataConverter;
+import org.openmrs.module.reporting.data.converter.ObjectFormatter;
+import org.openmrs.module.reporting.data.patient.definition.ConvertedPatientDataDefinition;
+import org.openmrs.module.reporting.data.patient.definition.PatientIdentifierDataDefinition;
+import org.openmrs.module.reporting.data.person.definition.BirthdateDataDefinition;
+import org.openmrs.module.reporting.data.person.definition.ConvertedPersonDataDefinition;
+import org.openmrs.module.reporting.data.person.definition.GenderDataDefinition;
+import org.openmrs.module.reporting.data.person.definition.PersonIdDataDefinition;
+import org.openmrs.module.reporting.data.person.definition.PreferredNameDataDefinition;
+import org.openmrs.module.reporting.dataset.definition.DataSetDefinition;
+import org.openmrs.module.reporting.dataset.definition.PatientDataSetDefinition;
+import org.openmrs.module.reporting.evaluation.parameter.Mapped;
+import org.openmrs.module.reporting.evaluation.parameter.Parameter;
+import org.openmrs.module.reporting.report.definition.ReportDefinition;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
+
+@Component
+@Builds({ "ehraddons.common.report.malaria" })
+public class SetupMalariaReport extends AbstractHybridReportBuilder {
+	
+	public static final String DATE_FORMAT = "dd/MM/yyyy";
+	
+	private CommonDatasetDefinition commonDatasetDefinition;
+	
+	@Autowired
+	public SetupMalariaReport(CommonDatasetDefinition commonDatasetDefinition) {
+		this.commonDatasetDefinition = commonDatasetDefinition;
+	}
+	
+	@Override
+	protected Mapped<CohortDefinition> buildCohort(HybridReportDescriptor hybridReportDescriptor,
+	        PatientDataSetDefinition patientDataSetDefinition) {
+		return null;
+	}
+	
+	@Override
+	protected List<Parameter> getParameters(ReportDescriptor reportDescriptor) {
+		return Arrays.asList(new Parameter("startDate", "Start Date", Date.class), new Parameter("endDate", "End Date",
+		        Date.class), new Parameter("dateBasedReporting", "", String.class));
+	}
+	
+	@Override
+	protected void addColumns(HybridReportDescriptor report, PatientDataSetDefinition dsd) {
+	}
+	
+	@Override
+	protected List<Mapped<DataSetDefinition>> buildDataSets(ReportDescriptor descriptor, ReportDefinition report) {
+		
+		PatientDataSetDefinition allVisits = allPatients();
+		allVisits.addRowFilter(allMalariaPatientsCohort());
+		
+		return Arrays.asList(ReportUtils.map((DataSetDefinition) allVisits, "startDate=${startDate},endDate=${endDate}"),
+		    ReportUtils.map(commonDatasetDefinition.getFacilityMetadata(), ""));
+	}
+	
+	protected PatientDataSetDefinition allPatients() {
+		PatientDataSetDefinition dsd = new PatientDataSetDefinition();
+		dsd.setName("all");
+		dsd.addParameter(new Parameter("startDate", "Start Date", Date.class));
+		dsd.addParameter(new Parameter("endDate", "End Date", Date.class));
+		String defParam = "startDate=${startDate},endDate=${endDate}";
+		
+		PatientIdentifierType upn = MetadataUtils.existing(PatientIdentifierType.class,
+		    HivMetadata._PatientIdentifierType.UNIQUE_PATIENT_NUMBER);
+		DataConverter identifierFormatter = new ObjectFormatter("{identifier}");
+		DataDefinition identifierDef = new ConvertedPatientDataDefinition("identifier", new PatientIdentifierDataDefinition(
+		        upn.getName(), upn), identifierFormatter);
+		
+		DataConverter formatter = new ObjectFormatter("{familyName}, {givenName}");
+		DataDefinition nameDef = new ConvertedPersonDataDefinition("name", new PreferredNameDataDefinition(), formatter);
+		dsd.addColumn("id", new PersonIdDataDefinition(), "");
+		dsd.addColumn("Name", nameDef, "");
+		dsd.addColumn("Identifier", identifierDef, "");
+		dsd.addColumn("Sex", new GenderDataDefinition(), "", null);
+		dsd.addColumn("DOB", new BirthdateDataDefinition(), "", new BirthdateConverter(DATE_FORMAT));
+		return dsd;
+	}
+	
+	protected Mapped<CohortDefinition> allMalariaPatientsCohort() {
+		SqlCohortDefinition cd = new SqlCohortDefinition();
+		cd.addParameter(new Parameter("startDate", "Start Date", Date.class));
+		cd.addParameter(new Parameter("endDate", "End Date", Date.class));
+		cd.setName("Active Patients");
+		cd.setQuery("SELECT p.patient_id FROM patient p INNER JOIN encounter e ON p.patient_id=e.patient_id WHERE e.encounter_datetime BETWEEN :startDate AND :endDate ");
+		return ReportUtils.map((CohortDefinition) cd, "startDate=${startDate},endDate=${endDate}");
+	}
+}
