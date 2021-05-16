9263a6bf57d1396ac6118609781332412e2ed7a5
ningosi <nicotwendelee@gmail.com>
2021-05-16 19:42:47 +0300

    Filter only todays records on the view for the patient bills

--- omod/src/main/java/org/openmrs/module/financials/fragment/controller/SummaryFragmentController.java
+++ omod/src/main/java/org/openmrs/module/financials/fragment/controller/SummaryFragmentController.java
@@ -1,6 +1,5 @@
 package org.openmrs.module.financials.fragment.controller;
 
-import org.apache.commons.lang.StringUtils;
 import org.openmrs.PersonAttributeType;
 import org.openmrs.api.PersonService;
 import org.openmrs.api.context.Context;
@@ -11,11 +10,11 @@ import org.openmrs.module.hospitalcore.HospitalCoreService;
 import org.openmrs.module.hospitalcore.model.OpdTestOrder;
 import org.openmrs.module.hospitalcore.model.PatientServiceBill;
 import org.openmrs.module.hospitalcore.model.PatientServiceBillItem;
-import org.openmrs.ui.framework.UiUtils;
 import org.openmrs.ui.framework.fragment.FragmentModel;
-import org.openmrs.ui.framework.page.PageModel;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 public class SummaryFragmentController {
@@ -24,6 +23,7 @@ public class SummaryFragmentController {
 		List<PatientServiceBill> allBils = Context.getService(BillingService.class).getAllPatientServiceBill();
 		PersonService personService = Context.getPersonService();
 		List<PatientBillSummary> allBills = new ArrayList<PatientBillSummary>();
+		SimpleDateFormat formatterDate = new SimpleDateFormat("yyyy-MM-dd");
 		
 		//get person attribute for patient category and sub category
 		PersonAttributeType paymentCategory = personService
@@ -32,27 +32,32 @@ public class SummaryFragmentController {
 		        .getPersonAttributeTypeByUuid("972a32aa-6159-11eb-bc2d-9785fed39154");
 		
 		for (PatientServiceBill patientServiceBill : allBils) {
-			PatientBillSummary patientBillSummary = new PatientBillSummary();
-			patientBillSummary.setBillId(patientServiceBill.getPatientServiceBillId());
-			patientBillSummary.setPatient(patientServiceBill.getPatient().getPersonName().getFullName());
-			if (patientServiceBill.getPatient().getAttribute(paymentCategory) != null) {
-				patientBillSummary.setCategory(patientServiceBill.getPatient().getAttribute(paymentCategory).getValue());
-			} else {
-				patientBillSummary.setCategory("");
+			String createdDate = formatterDate.format(patientServiceBill.getCreatedDate());
+			String today = formatterDate.format(new Date());
+			PatientBillSummary patientBillSummary = null;
+			if (createdDate.equals(today)) {
+				patientBillSummary = new PatientBillSummary();
+				patientBillSummary.setBillId(patientServiceBill.getPatientServiceBillId());
+				patientBillSummary.setPatient(patientServiceBill.getPatient().getPersonName().getFullName());
+				if (patientServiceBill.getPatient().getAttribute(paymentCategory) != null) {
+					patientBillSummary.setCategory(patientServiceBill.getPatient().getAttribute(paymentCategory).getValue());
+				} else {
+					patientBillSummary.setCategory("");
+				}
+				if (patientServiceBill.getPatient().getAttribute(paymentSubCategory) != null) {
+					patientBillSummary.setSubCategory(patientServiceBill.getPatient().getAttribute(paymentSubCategory)
+					        .getValue());
+				} else {
+					patientBillSummary.setSubCategory("");
+				}
+				patientBillSummary.setWaiver(String.valueOf(patientServiceBill.getWaiverAmount()));
+				patientBillSummary.setActualAmount(String.valueOf(patientServiceBill.getActualAmount()));
+				patientBillSummary.setPaidAmount(String.valueOf(patientServiceBill.getAmount()));
+				patientBillSummary.setRebate(String.valueOf(patientServiceBill.getRebateAmount()));
+				patientBillSummary.setTransactionDate(String.valueOf(patientServiceBill.getReceipt().getPaidDate()));
+				patientBillSummary.setIdentifier(patientServiceBill.getPatient().getPatientIdentifier().getIdentifier());
+				//add this build object to the list
 			}
-			if (patientServiceBill.getPatient().getAttribute(paymentSubCategory) != null) {
-				patientBillSummary.setSubCategory(patientServiceBill.getPatient().getAttribute(paymentSubCategory)
-				        .getValue());
-			} else {
-				patientBillSummary.setSubCategory("");
-			}
-			patientBillSummary.setWaiver(String.valueOf(patientServiceBill.getWaiverAmount()));
-			patientBillSummary.setActualAmount(String.valueOf(patientServiceBill.getActualAmount()));
-			patientBillSummary.setPaidAmount(String.valueOf(patientServiceBill.getAmount()));
-			patientBillSummary.setRebate(String.valueOf(patientServiceBill.getRebateAmount()));
-			patientBillSummary.setTransactionDate(String.valueOf(patientServiceBill.getReceipt().getPaidDate()));
-			patientBillSummary.setIdentifier(patientServiceBill.getPatient().getPatientIdentifier().getIdentifier());
-			//add this build object to the list
 			allBills.add(patientBillSummary);
 		}
 		
