bfd880f02bee530c92e94d837e9d4bad313d6c07
ningosi <nicotwendelee@gmail.com>
2021-06-11 10:48:44 +0300

    Prepare for the next release life cycle

--- api/src/main/java/org/openmrs/module/financials/reports/SetupMOH717Report.java
+++ api/src/main/java/org/openmrs/module/financials/reports/SetupMOH717Report.java
@@ -17,6 +17,7 @@ import java.util.Date;
 import java.util.List;
 
 import static org.openmrs.module.kenyacore.report.ReportUtils.map;
+
 @Component
 @Builds({ "ehraddons.common.717" })
 public class SetupMOH717Report extends AbstractReportBuilder {
