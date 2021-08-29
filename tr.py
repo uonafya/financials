1be5e80f9a2f4ccf0f3de5b0bafb57d89942bb65
vinchuli <mavenwechuli@gmail.com>
2021-08-29 17:04:54 -0400

    EhrAddonsConstants For DIABETES

--- tr.py
+++
@@ -1,8 +0,0 @@
-# data = 'diabe.csv'
-for x in range(1,214):
-    print(f'getConcept(EhrAddonsConstants._EhrAddOnConcepts.DIABETES_{x}).getConceptId()')
-    print(f'getConcept(EhrAddonsConstants._EhrAddOnConcepts.DIABETES_{x}).getConceptId()')
-    print(f'getConcept(EhrAddonsConstants._EhrAddOnConcepts.DIABETES_{x}).getConceptId()')
-    print(f'getConcept(EhrAddonsConstants._EhrAddOnConcepts.DIABETES_{x}).getConceptId()')
-
-    
