7f8a0adff56ea0b3598e471197676691160b6e91
ningosi <nicotwendelee@gmail.com>
2021-08-30 23:04:05 +0300

    Add trypanosomiasis diagnosis constant concepts

--- api/src/main/java/org/openmrs/module/financials/diagnosis/lists/DiagnosisLists.java
+++ api/src/main/java/org/openmrs/module/financials/diagnosis/lists/DiagnosisLists.java
@@ -6135,16 +6135,15 @@ public class DiagnosisLists {
 	}
 	
 	public static List<Integer> getTryponomiasisList() {
-		return Arrays.asList(getConcept(EhrAddonsConstants._EhrAddOnConcepts.ACUTE_MILIARY_TUBERCULOSIS_UNSPECIFIED)
+		return Arrays.asList(getConcept(EhrAddonsConstants._EhrAddOnConcepts.TRYPANOSOMIASIS1)
 		        .getConceptId(),
-		    getConcept(EhrAddonsConstants._EhrAddOnConcepts.EXTRAPULMONARY_TUBERCULOSIS).getConceptId(),
-		    getConcept(EhrAddonsConstants._EhrAddOnConcepts.MULTI_DRUG_RESISTANT_TUBERCULOSIS).getConceptId(),
-		    getConcept(EhrAddonsConstants._EhrAddOnConcepts.PULMONARY_TUBERCULOSIS).getConceptId(),
-		    getConcept(EhrAddonsConstants._EhrAddOnConcepts.TUBERCULOSIS_OF_THE_GENITOURINARY_SYSTEM).getConceptId(),
-		    getConcept(EhrAddonsConstants._EhrAddOnConcepts.TUBERCULOSIS_OF_OTHER_ORGANS).getConceptId(),
-		    getConcept(EhrAddonsConstants._EhrAddOnConcepts.TUBERCULOSIS_IN_PREGNANCY).getConceptId(),
-		    getConcept(EhrAddonsConstants._EhrAddOnConcepts.ACUTE_MILIARY_TUBERCULOSIS_UNSPECIFIED).getConceptId(),
-		    getConcept(EhrAddonsConstants._EhrAddOnConcepts.TUBERCULOSIS).getConceptId()
+		    getConcept(EhrAddonsConstants._EhrAddOnConcepts.TRYPANOSOMIASIS2).getConceptId(),
+		    getConcept(EhrAddonsConstants._EhrAddOnConcepts.TRYPANOSOMIASIS3).getConceptId(),
+		    getConcept(EhrAddonsConstants._EhrAddOnConcepts.TRYPANOSOMIASIS4).getConceptId(),
+		    getConcept(EhrAddonsConstants._EhrAddOnConcepts.TRYPANOSOMIASIS5).getConceptId(),
+		    getConcept(EhrAddonsConstants._EhrAddOnConcepts.TRYPANOSOMIASIS6).getConceptId(),
+		    getConcept(EhrAddonsConstants._EhrAddOnConcepts.TRYPANOSOMIASIS7).getConceptId(),
+		    getConcept(EhrAddonsConstants._EhrAddOnConcepts.TRYPANOSOMIASIS8).getConceptId()
 		
 		);
 	}
