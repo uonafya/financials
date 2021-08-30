0aea31b8021c076e39d0993c0fbf52dda9b9424b
ningosi <nicotwendelee@gmail.com>
2021-08-30 22:40:16 +0300

    Add the rickets diagnosis constants

--- api/src/main/java/org/openmrs/module/financials/diagnosis/lists/DiagnosisLists.java
+++ api/src/main/java/org/openmrs/module/financials/diagnosis/lists/DiagnosisLists.java
@@ -7,6 +7,7 @@ import org.openmrs.module.financials.MascularDiagnosisConstants;
 import org.openmrs.module.financials.MentalPalsyDiagnosisConstants;
 import org.openmrs.module.financials.OncologyDiagnosisConstants;
 import org.openmrs.module.financials.OtherBitesDiagnosisConstants;
+import org.openmrs.module.financials.RicketsDiagnosisConstants;
 
 import java.util.Arrays;
 import java.util.List;
@@ -6329,16 +6330,21 @@ public class DiagnosisLists {
 	}
 	
 	public static List<Integer> getRicketsList() {
-		return Arrays.asList(getConcept(EhrAddonsConstants._EhrAddOnConcepts.ACUTE_MILIARY_TUBERCULOSIS_UNSPECIFIED)
-		        .getConceptId(),
-		    getConcept(EhrAddonsConstants._EhrAddOnConcepts.EXTRAPULMONARY_TUBERCULOSIS).getConceptId(),
-		    getConcept(EhrAddonsConstants._EhrAddOnConcepts.MULTI_DRUG_RESISTANT_TUBERCULOSIS).getConceptId(),
-		    getConcept(EhrAddonsConstants._EhrAddOnConcepts.PULMONARY_TUBERCULOSIS).getConceptId(),
-		    getConcept(EhrAddonsConstants._EhrAddOnConcepts.TUBERCULOSIS_OF_THE_GENITOURINARY_SYSTEM).getConceptId(),
-		    getConcept(EhrAddonsConstants._EhrAddOnConcepts.TUBERCULOSIS_OF_OTHER_ORGANS).getConceptId(),
-		    getConcept(EhrAddonsConstants._EhrAddOnConcepts.TUBERCULOSIS_IN_PREGNANCY).getConceptId(),
-		    getConcept(EhrAddonsConstants._EhrAddOnConcepts.ACUTE_MILIARY_TUBERCULOSIS_UNSPECIFIED).getConceptId(),
-		    getConcept(EhrAddonsConstants._EhrAddOnConcepts.TUBERCULOSIS).getConceptId()
+		return Arrays.asList(getConcept(RicketsDiagnosisConstants.RICKETS1).getConceptId(),
+		    getConcept(RicketsDiagnosisConstants.RICKETS2).getConceptId(), getConcept(RicketsDiagnosisConstants.RICKETS3)
+		            .getConceptId(), getConcept(RicketsDiagnosisConstants.RICKETS4).getConceptId(),
+		    getConcept(RicketsDiagnosisConstants.RICKETS5).getConceptId(), getConcept(RicketsDiagnosisConstants.RICKETS6)
+		            .getConceptId(), getConcept(RicketsDiagnosisConstants.RICKETS7).getConceptId(),
+		    getConcept(RicketsDiagnosisConstants.RICKETS8).getConceptId(), getConcept(RicketsDiagnosisConstants.RICKETS9)
+		            .getConceptId(), getConcept(RicketsDiagnosisConstants.RICKETS10).getConceptId(),
+		    getConcept(RicketsDiagnosisConstants.RICKETS11).getConceptId(), getConcept(RicketsDiagnosisConstants.RICKETS12)
+		            .getConceptId(), getConcept(RicketsDiagnosisConstants.RICKETS13).getConceptId(),
+		    getConcept(RicketsDiagnosisConstants.RICKETS14).getConceptId(), getConcept(RicketsDiagnosisConstants.RICKETS15)
+		            .getConceptId(), getConcept(RicketsDiagnosisConstants.RICKETS16).getConceptId(),
+		    getConcept(RicketsDiagnosisConstants.RICKETS17).getConceptId(), getConcept(RicketsDiagnosisConstants.RICKETS18)
+		            .getConceptId(), getConcept(RicketsDiagnosisConstants.RICKETS19).getConceptId(),
+		    getConcept(RicketsDiagnosisConstants.RICKETS20).getConceptId(), getConcept(RicketsDiagnosisConstants.RICKETS21)
+		            .getConceptId()
 		
 		);
 	}
