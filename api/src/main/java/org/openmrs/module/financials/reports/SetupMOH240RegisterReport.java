fa2cdd39960b6bab6c7edc0166a9b7e41a404161
ningosi <nicotwendelee@gmail.com>
2021-09-21 22:45:41 +0300

    Add base cohort for the lab register and map the fields on the template

--- api/src/main/java/org/openmrs/module/financials/reports/SetupMOH240RegisterReport.java
+++ api/src/main/java/org/openmrs/module/financials/reports/SetupMOH240RegisterReport.java
@@ -1,20 +1,43 @@
 package org.openmrs.module.financials.reports;
 
+import org.openmrs.Concept;
+import org.openmrs.EncounterType;
+import org.openmrs.PatientIdentifierType;
+import org.openmrs.api.context.Context;
+import org.openmrs.module.financials.EhrAddonsConstants;
+import org.openmrs.module.financials.reporting.calculation.RevisitPatientCalculation;
+import org.openmrs.module.financials.reporting.calculation.VillageAndLandmarkCalculation;
+import org.openmrs.module.financials.reporting.converter.EncounterDateConverter;
 import org.openmrs.module.financials.reporting.library.dataset.CommonDatasetDefinition;
 import org.openmrs.module.kenyacore.report.HybridReportDescriptor;
 import org.openmrs.module.kenyacore.report.ReportDescriptor;
 import org.openmrs.module.kenyacore.report.ReportUtils;
 import org.openmrs.module.kenyacore.report.builder.AbstractHybridReportBuilder;
 import org.openmrs.module.kenyacore.report.builder.Builds;
+import org.openmrs.module.kenyacore.report.data.patient.definition.CalculationDataDefinition;
+import org.openmrs.module.kenyaemr.calculation.library.TelephoneNumberCalculation;
+import org.openmrs.module.kenyaemr.metadata.CommonMetadata;
 import org.openmrs.module.kenyaemr.reporting.calculation.converter.GenderConverter;
+import org.openmrs.module.kenyaemr.reporting.data.converter.CalculationResultConverter;
+import org.openmrs.module.kenyaemr.reporting.data.converter.IdentifierConverter;
+import org.openmrs.module.metadatadeploy.MetadataUtils;
 import org.openmrs.module.reporting.cohort.definition.CohortDefinition;
+import org.openmrs.module.reporting.cohort.definition.SqlCohortDefinition;
+import org.openmrs.module.reporting.common.TimeQualifier;
 import org.openmrs.module.reporting.data.DataDefinition;
 import org.openmrs.module.reporting.data.converter.BirthdateConverter;
 import org.openmrs.module.reporting.data.converter.DataConverter;
 import org.openmrs.module.reporting.data.converter.ObjectFormatter;
+import org.openmrs.module.reporting.data.converter.ObsValueConverter;
+import org.openmrs.module.reporting.data.patient.definition.ConvertedPatientDataDefinition;
+import org.openmrs.module.reporting.data.patient.definition.EncountersForPatientDataDefinition;
+import org.openmrs.module.reporting.data.patient.definition.PatientIdentifierDataDefinition;
+import org.openmrs.module.reporting.data.patient.definition.SqlPatientDataDefinition;
+import org.openmrs.module.reporting.data.person.definition.AgeDataDefinition;
 import org.openmrs.module.reporting.data.person.definition.BirthdateDataDefinition;
 import org.openmrs.module.reporting.data.person.definition.ConvertedPersonDataDefinition;
 import org.openmrs.module.reporting.data.person.definition.GenderDataDefinition;
+import org.openmrs.module.reporting.data.person.definition.ObsForPersonDataDefinition;
 import org.openmrs.module.reporting.data.person.definition.PersonIdDataDefinition;
 import org.openmrs.module.reporting.data.person.definition.PreferredNameDataDefinition;
 import org.openmrs.module.reporting.dataset.definition.DataSetDefinition;
@@ -50,10 +73,11 @@ public class SetupMOH240RegisterReport extends AbstractHybridReportBuilder {
 	
 	@Override
 	protected List<Mapped<DataSetDefinition>> buildDataSets(ReportDescriptor descriptor, ReportDefinition report) {
-		PatientDataSetDefinition dsd = ipdList();
+		PatientDataSetDefinition dsd = LabRegister();
 		dsd.setName("lrr");
 		dsd.addParameter(new Parameter("startDate", "Start Date", Date.class));
 		dsd.addParameter(new Parameter("endDate", "End Date", Date.class));
+		report.setBaseCohortDefinition(ReportUtils.map(getBaseCohort(), "startDate=${startDate},endDate=${endDate}"));
 		
 		return Arrays.asList(ReportUtils.map((DataSetDefinition) dsd, "startDate=${startDate},endDate=${endDate}"),
 		    ReportUtils.map(commonDatasetDefinition.getFacilityMetadata(), ""));
@@ -65,15 +89,71 @@ public class SetupMOH240RegisterReport extends AbstractHybridReportBuilder {
 		        Date.class), new Parameter("dateBasedReporting", "", String.class));
 	}
 	
-	private PatientDataSetDefinition ipdList() {
+	private PatientDataSetDefinition LabRegister() {
 		PatientDataSetDefinition dsd = new PatientDataSetDefinition();
-		DataConverter nameFormatter = new ObjectFormatter("{familyName}, {givenName}");
+		DataConverter nameFormatter = new ObjectFormatter("{familyName}, {givenName}, {middleName}");
 		DataDefinition nameDef = new ConvertedPersonDataDefinition("name", new PreferredNameDataDefinition(), nameFormatter);
+		
+		PatientIdentifierType opdNumber = MetadataUtils.existing(PatientIdentifierType.class,
+		    CommonMetadata._PatientIdentifierType.PATIENT_CLINIC_NUMBER);
+		DataDefinition identifierDef = new ConvertedPatientDataDefinition("identifier", new PatientIdentifierDataDefinition(
+		        opdNumber.getName(), opdNumber), new IdentifierConverter());
+		
 		dsd.addColumn("id", new PersonIdDataDefinition(), "");
+		dsd.addColumn("identifier", identifierDef, "");
+		dsd.addColumn("Date", getEncounterDate(), "onOrAfter=${startDate},onOrBefore=${endDate+23h}",
+		    new EncounterDateConverter());
 		dsd.addColumn("Name", nameDef, "");
-		dsd.addColumn("Sex", new GenderDataDefinition(), "", new GenderConverter());
-		dsd.addColumn("Date of Birth", new BirthdateDataDefinition(), "", new BirthdateConverter(DATE_FORMAT));
+		dsd.addColumn("Sex", new GenderDataDefinition(), "", null);
+		dsd.addColumn("DOB", new BirthdateDataDefinition(), "", new BirthdateConverter(DATE_FORMAT));
+		dsd.addColumn("age", new AgeDataDefinition(), "");
+		dsd.addColumn("village", new CalculationDataDefinition("village", new VillageAndLandmarkCalculation()), "",
+		    new CalculationResultConverter());
+		dsd.addColumn("telephone", new CalculationDataDefinition("telephone", new TelephoneNumberCalculation()), "",
+		    new CalculationResultConverter());
+		dsd.addColumn("RVT", getRevisit(), "endDate=${endDate+23h}", new CalculationResultConverter());
+		dsd.addColumn("DIAG",
+		    getObservation(EhrAddonsConstants.getConcept(EhrAddonsConstants._EhrAddOnConcepts.FINA_DIAGNOSIS)),
+		    "onOrAfter=${startDate},onOrBefore=${endDate+23h}", new ObsValueConverter());
 		return dsd;
+		
+	}
+	
+	private DataDefinition getEncounterDate() {
+		EncountersForPatientDataDefinition dsd = new EncountersForPatientDataDefinition();
+		dsd.addParameter(new Parameter("onOrAfter", "After Date", Date.class));
+		dsd.addParameter(new Parameter("onOrBefore", "Before Date", Date.class));
+		dsd.setWhich(TimeQualifier.LAST);
+		return dsd;
+	}
+	
+	private CohortDefinition getBaseCohort() {
+		EncounterType labEncounter = Context.getEncounterService().getEncounterTypeByUuid(
+		    "11d3f37a-f282-11ea-a825-1b5b1ff1b854");
+		SqlCohortDefinition cd = new SqlCohortDefinition();
+		cd.addParameter(new Parameter("startDate", "Start Date", Date.class));
+		cd.addParameter(new Parameter("endDate", "End Date", Date.class));
+		cd.setName("Base cohort for the lab encounter");
+		cd.setQuery("SELECT p.patient_id FROM patient p INNER JOIN encounter e ON p.patient_id=e.patient_id"
+		        + " WHERE e.encounter_datetime BETWEEN :startDate AND :endDate AND p.voided=0 AND e.voided=0 "
+		        + " AND e.encounter_type=" + labEncounter.getEncounterTypeId());
+		return cd;
+	}
+	
+	private DataDefinition getRevisit() {
+		CalculationDataDefinition cd = new CalculationDataDefinition("RVT", new RevisitPatientCalculation());
+		cd.addParameter(new Parameter("endDate", "End Date", Date.class));
+		return cd;
+		
+	}
+	
+	private DataDefinition getObservation(Concept question) {
+		ObsForPersonDataDefinition obs = new ObsForPersonDataDefinition();
+		obs.addParameter(new Parameter("onOrAfter", "After Date", Date.class));
+		obs.addParameter(new Parameter("onOrBefore", "Before Date", Date.class));
+		obs.setWhich(TimeQualifier.LAST);
+		obs.setQuestion(question);
+		return obs;
 		
 	}
 }
