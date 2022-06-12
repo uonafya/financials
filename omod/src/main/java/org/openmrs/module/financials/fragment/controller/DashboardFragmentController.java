aaf646af8f14f31ac52a34525a92e05539a9f3e8
ningosi <nicotwendelee@gmail.com>
2022-06-12 14:46:34 +0300

    Redirect the invoice the requred page

--- omod/src/main/java/org/openmrs/module/financials/fragment/controller/DashboardFragmentController.java
+++ omod/src/main/java/org/openmrs/module/financials/fragment/controller/DashboardFragmentController.java
@@ -1,7 +1,6 @@
 package org.openmrs.module.financials.fragment.controller;
 
 import org.openmrs.api.context.Context;
-import org.openmrs.module.financials.utils.FinancialsUtils;
 import org.openmrs.module.hospitalcore.HospitalCoreService;
 import org.openmrs.module.hospitalcore.model.EhrDepartment;
 import org.openmrs.module.hospitalcore.model.PatientServiceBillItem;
@@ -10,41 +9,13 @@ import org.openmrs.ui.framework.fragment.FragmentModel;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.text.ParseException;
-import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
 
 public class DashboardFragmentController {
 	
-	public void controller(FragmentModel model) {
-		HospitalCoreService hospitalCoreService = Context.getService(HospitalCoreService.class);
-		Date startOfDay = FinancialsUtils.getStartOfDay(new Date());
-		Date endOfDay = FinancialsUtils.getEndOfDay(new Date());
-		
-		model.addAttribute(
-		    "registration",
-		    getDepartmentTotalsOnDateRange(hospitalCoreService, startOfDay, endOfDay,
-		        hospitalCoreService.getDepartmentByName("Registration")));
-		model.addAttribute(
-		    "laboratory",
-		    getDepartmentTotalsOnDateRange(hospitalCoreService, startOfDay, endOfDay,
-		        hospitalCoreService.getDepartmentByName("Laboratory")));
-		model.addAttribute(
-		    "radiology",
-		    getDepartmentTotalsOnDateRange(hospitalCoreService, startOfDay, endOfDay,
-		        hospitalCoreService.getDepartmentByName("Radiology")));
-		model.addAttribute(
-		    "pharmacy",
-		    getDepartmentTotalsOnDateRange(hospitalCoreService, startOfDay, endOfDay,
-		        hospitalCoreService.getDepartmentByName("Pharmacy")));
-		model.addAttribute(
-		    "nhif",
-		    getDepartmentTotalsOnDateRange(hospitalCoreService, startOfDay, endOfDay,
-		        hospitalCoreService.getDepartmentByName("NHIF")));
-		model.addAttribute(
-		    "procedure",
-		    getDepartmentTotalsOnDateRange(hospitalCoreService, startOfDay, endOfDay,
-		        hospitalCoreService.getDepartmentByName("Procedure")));
+	public void controller(FragmentModel model) throws ParseException {
+		//getDepartmentTotalsOnDateRange(null,null);
 	}
 	
 	private Double getDepartmentTotalsOnDateRange(HospitalCoreService hospitalCoreService, Date startDate, Date endDate,
@@ -62,13 +33,9 @@ public class DashboardFragmentController {
 		return totals;
 	}
 	
-	public SimpleObject getDepartmentTotalsOnDateRange(
-	        @RequestParam(value = "startDate", required = false) String startdate,
-	        @RequestParam(value = "endDate", required = false) String enddate) throws ParseException {
+	public SimpleObject getDepartmentTotalsOnDateRange(@RequestParam(value = "fromDate", required = false) Date startDate,
+	        @RequestParam(value = "toDate", required = false) Date endDate) throws ParseException {
 		HospitalCoreService hospitalCoreService = Context.getService(HospitalCoreService.class);
-		
-		Date startDate = new SimpleDateFormat("dd MMM yyyy").parse(startdate);
-		Date endDate = new SimpleDateFormat("dd MMM yyyy").parse(enddate);
 		
 		SimpleObject simpleObject = new SimpleObject();
 		
