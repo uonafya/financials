6500a9454ec8e6dfcb50d313475b93f3e3eddf59
ningosi <nicotwendelee@gmail.com>
2022-09-20 16:12:17 +0300

    Add more metrics to pull summaries for the patients on the revenue dashboard

--- omod/src/main/java/org/openmrs/module/financials/fragment/controller/WorkLoadFragmentController.java
+++ omod/src/main/java/org/openmrs/module/financials/fragment/controller/WorkLoadFragmentController.java
@@ -5,6 +5,8 @@ import org.openmrs.EncounterType;
 import org.openmrs.Obs;
 import org.openmrs.api.context.Context;
 import org.openmrs.module.financials.utils.FinancialsUtils;
+import org.openmrs.module.hospitalcore.IpdService;
+import org.openmrs.module.hospitalcore.model.IpdPatientAdmitted;
 import org.openmrs.parameter.EncounterSearchCriteria;
 import org.openmrs.parameter.EncounterSearchCriteriaBuilder;
 import org.openmrs.ui.framework.SimpleObject;
@@ -34,24 +36,91 @@ public class WorkLoadFragmentController {
 			endOfDay = FinancialsUtils.getEndOfDay(endDate);
 		}
 		
-		Integer opd = 0;
-		Integer specialClinic = 0;
-		Integer ipd = 0;
-		Integer mopc = 0;
-		Integer lab = 0;
-		Integer procedure = 0;
-		Integer radiology = 0;
+		int opd = 0;
+		int specialClinic = 0;
+		int ipd = 0;
+		int mopc = 0;
+		int lab = 0;
+		int procedure = 0;
+		int radiology = 0;
+		int dental = 0;
+		int eye = 0;
+		int ent = 0;
+		int mchMothers = 0;
+		int mchChildren = 0;
+		int deliveries = 0;
+		int immunization = 0;
+		int anc = 0;
+		int pnc = 0;
+		int preventiveServices = 0;
 		
 		EncounterType opdEncounterType = Context.getEncounterService().getEncounterTypeByUuid(
 		    "ba45c278-f290-11ea-9666-1b3e6e848887");
+		EncounterType labEncounterType = Context.getEncounterService().getEncounterTypeByUuid(
+		    "11d3f37a-f282-11ea-a825-1b5b1ff1b854");
+		EncounterType radiologyEncounterType = Context.getEncounterService().getEncounterTypeByUuid(
+		    "012bb9f4-f282-11ea-a6d6-3b4fa4aefb5a");
+		EncounterType mchMotherEnrollmentEncounterType = Context.getEncounterService().getEncounterTypeByUuid(
+		    "3ee036d8-7c13-4393-b5d6-036f2fe45126");
+		EncounterType mchChildEnrollmentEncounterType = Context.getEncounterService().getEncounterTypeByUuid(
+		    "8553d869-bdc8-4287-8505-910c7c998aff");
+		EncounterType mchMotherFollowupEncounterType = Context.getEncounterService().getEncounterTypeByUuid(
+		    "c6d09e05-1f25-4164-8860-9f32c5a02df0");
+		EncounterType mchChildFollowupEncounterType = Context.getEncounterService().getEncounterTypeByUuid(
+		    "755b59e6-acbb-4853-abaf-be302039f902");
+		EncounterType mchChildImmunizationEncounterType = Context.getEncounterService().getEncounterTypeByUuid(
+		    "b4f3859e-861c-4a63-bdff-eb7392030d47");
+		EncounterType ancEncounterType = Context.getEncounterService().getEncounterTypeByUuid(
+		    "e8f98494-af35-4bb8-9fc7-c409c8fed843");
+		EncounterType pncEncounterType = Context.getEncounterService().getEncounterTypeByUuid(
+		    "72aa78e0-ee4b-47c3-9073-26f3b9ecc4a7");
+		EncounterType deliveriesEncounterType = Context.getEncounterService().getEncounterTypeByUuid(
+		    "496c7cc3-0eea-4e84-a04c-2292949e2f7f");
+		EncounterType preventiveServicesEncounterType = Context.getEncounterService().getEncounterTypeByUuid(
+		    "d3ea25c7-a3e8-4f57-a6a9-e802c3565a30");
 		
 		List<Encounter> getOpdEncounters = Context.getEncounterService().getEncounters(
-		    formEncounterSearchCriteria(startOfDay, endOfDay, opdEncounterType));
+		    formEncounterSearchCriteria(startOfDay, endOfDay, Arrays.asList(opdEncounterType)));
 		List<Obs> getAllSpecialClinicObs = FinancialsUtils.getObsList(startOfDay, endOfDay,
 		    FinancialsUtils.getConcept("b5e0cfd3-1009-4527-8e36-83b5e902b3ea"));
 		List<Obs> getAllMopcObs = FinancialsUtils.getObsList(startOfDay, endOfDay,
 		    FinancialsUtils.getConcept("03880388-07ce-4961-abe7-0e58f787dd23"),
 		    FinancialsUtils.getConcept("66710a6d-5894-4f7d-a874-b449df77314d"));
+		List<Obs> getAllSpecialClinicDentalObs = FinancialsUtils.getObsList(startOfDay, endOfDay,
+		    FinancialsUtils.getConcept("b5e0cfd3-1009-4527-8e36-83b5e902b3ea"),
+		    FinancialsUtils.getConcept("30aff715-92de-4662-aa33-fa6b6179fed0"));
+		List<Obs> getAllSpecialClinicEyeObs = FinancialsUtils.getObsList(startOfDay, endOfDay,
+		    FinancialsUtils.getConcept("b5e0cfd3-1009-4527-8e36-83b5e902b3ea"),
+		    FinancialsUtils.getConcept("36f7a4c9-a64b-4351-bd08-fe185c7470dc"));
+		List<Obs> getAllSpecialClinicENTObs = FinancialsUtils.getObsList(startOfDay, endOfDay,
+		    FinancialsUtils.getConcept("b5e0cfd3-1009-4527-8e36-83b5e902b3ea"),
+		    FinancialsUtils.getConcept("160455AAAAAAAAAAAAAAAAAAAAAAAAAAAAAA"));
+		List<Encounter> getLabEncounters = Context.getEncounterService().getEncounters(
+		    formEncounterSearchCriteria(startOfDay, endOfDay, Arrays.asList(labEncounterType)));
+		List<Obs> getAllProceduresObs = FinancialsUtils.getObsList(startOfDay, endOfDay,
+		    FinancialsUtils.getConcept("1651AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA"));
+		List<Encounter> getRadiologyEncounters = Context.getEncounterService().getEncounters(
+		    formEncounterSearchCriteria(startOfDay, endOfDay, Arrays.asList(radiologyEncounterType)));
+		
+		List<Encounter> getMchMotherEncounters = Context.getEncounterService().getEncounters(
+		    formEncounterSearchCriteria(startOfDay, endOfDay,
+		        Arrays.asList(mchMotherEnrollmentEncounterType, mchMotherFollowupEncounterType)));
+		List<Encounter> getMchChildEncounters = Context.getEncounterService().getEncounters(
+		    formEncounterSearchCriteria(startOfDay, endOfDay,
+		        Arrays.asList(mchChildEnrollmentEncounterType, mchChildFollowupEncounterType)));
+		List<Encounter> getDeliveriesEncounters = Context.getEncounterService().getEncounters(
+		    formEncounterSearchCriteria(startOfDay, endOfDay, Arrays.asList(deliveriesEncounterType)));
+		List<Encounter> getImmunizationEncounters = Context.getEncounterService().getEncounters(
+		    formEncounterSearchCriteria(startOfDay, endOfDay, Arrays.asList(mchChildImmunizationEncounterType)));
+		List<Encounter> getAncEncounters = Context.getEncounterService().getEncounters(
+		    formEncounterSearchCriteria(startOfDay, endOfDay, Arrays.asList(ancEncounterType)));
+		List<Encounter> getpncEncounters = Context.getEncounterService().getEncounters(
+		    formEncounterSearchCriteria(startOfDay, endOfDay, Arrays.asList(pncEncounterType)));
+		List<Encounter> getPreventiveEncounters = Context.getEncounterService().getEncounters(
+		    formEncounterSearchCriteria(startOfDay, endOfDay, Arrays.asList(preventiveServicesEncounterType)));
+		
+		List<IpdPatientAdmitted> getAllAdmittedPatients = Context.getService(IpdService.class)
+		        .getAdmittedPatientsByDateRange(startOfDay, endOfDay);
 		
 		if (!getOpdEncounters.isEmpty()) {
 			opd = getOpdEncounters.size();
@@ -62,6 +131,48 @@ public class WorkLoadFragmentController {
 		if (!getAllMopcObs.isEmpty()) {
 			mopc = getAllMopcObs.size();
 		}
+		if (!getLabEncounters.isEmpty()) {
+			lab = getLabEncounters.size();
+		}
+		if (!getAllProceduresObs.isEmpty()) {
+			procedure = getAllProceduresObs.size();
+		}
+		if (!getRadiologyEncounters.isEmpty()) {
+			radiology = getRadiologyEncounters.size();
+		}
+		if (!getAllSpecialClinicDentalObs.isEmpty()) {
+			dental = getAllSpecialClinicDentalObs.size();
+		}
+		if (!getAllSpecialClinicEyeObs.isEmpty()) {
+			eye = getAllSpecialClinicEyeObs.size();
+		}
+		if (!getAllSpecialClinicENTObs.isEmpty()) {
+			ent = getAllSpecialClinicENTObs.size();
+		}
+		if (!getMchMotherEncounters.isEmpty()) {
+			mchMothers = getMchMotherEncounters.size();
+		}
+		if (!getMchChildEncounters.isEmpty()) {
+			mchChildren = getMchChildEncounters.size();
+		}
+		if (!getDeliveriesEncounters.isEmpty()) {
+			deliveries = getDeliveriesEncounters.size();
+		}
+		if (!getImmunizationEncounters.isEmpty()) {
+			immunization = getImmunizationEncounters.size();
+		}
+		if (!getAncEncounters.isEmpty()) {
+			anc = getAncEncounters.size();
+		}
+		if (!getpncEncounters.isEmpty()) {
+			pnc = getpncEncounters.size();
+		}
+		if (!getPreventiveEncounters.isEmpty()) {
+			preventiveServices = getPreventiveEncounters.size();
+		}
+		if (!getAllAdmittedPatients.isEmpty()) {
+			ipd = getAllAdmittedPatients.size();
+		}
 		
 		SimpleObject simpleObject = new SimpleObject();
 		simpleObject.put("opd", opd);
@@ -71,13 +182,25 @@ public class WorkLoadFragmentController {
 		simpleObject.put("lab", lab);
 		simpleObject.put("procedure", procedure);
 		simpleObject.put("radiology", radiology);
+		simpleObject.put("dental", dental);
+		simpleObject.put("eye", eye);
+		simpleObject.put("ent", ent);
+		
+		simpleObject.put("mchm", mchMothers);
+		simpleObject.put("mchc", mchChildren);
+		simpleObject.put("deliveries", deliveries);
+		simpleObject.put("immunization", immunization);
+		simpleObject.put("anc", anc);
+		simpleObject.put("pnc", pnc);
+		simpleObject.put("preventiveServices", preventiveServices);
 		
 		return simpleObject;
 		
 	}
 	
-	private EncounterSearchCriteria formEncounterSearchCriteria(Date startDate, Date endDate, EncounterType encounterType) {
-		return new EncounterSearchCriteriaBuilder().setEncounterTypes(Arrays.asList(encounterType)).setFromDate(startDate)
+	private EncounterSearchCriteria formEncounterSearchCriteria(Date startDate, Date endDate,
+	        List<EncounterType> encounterType) {
+		return new EncounterSearchCriteriaBuilder().setEncounterTypes(encounterType).setFromDate(startDate)
 		        .setToDate(endDate).setIncludeVoided(false).createEncounterSearchCriteria();
 	}
 }
